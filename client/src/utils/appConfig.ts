6f2e0cd95ed2420b35e65f068744980b94563dfe
Nitesh Baldaniya <baldaniyanitesh2003@gmail.com>
2026-04-23 15:21:58 +0530

    new disk

--- client/src/utils/appConfig.ts
+++ client/src/utils/appConfig.ts
@@ -1,11 +1,13 @@
+/** @format */
+
 export const APP_CONFIG = {
   API_URL: "https://devprojects.gitakshmi.com/api/v1",
   PMS_API_ROOT: "https://devprojects.gitakshmi.com",
-  SSO_LOGIN_URL: "https://gaccess.gitakshmi.com/login",
+  SSO_LOGIN_URL: "https://devgaccess.gitakshmi.com/login",
   SSO_APP: "pms",
   SSO_EXPECTED_ISS: "gitakshmi-sso",
   SSO_EXPECTED_AUD: "tms,pms",
   HRMS_URL: "https://hrms.dev.gitakshmi.com",
   AUTH_DEBUG: false,
-  SSO_CALLBACK_URL: "https://devprojects.gitakshmi.com/sso/callback"
+  SSO_CALLBACK_URL: "https://devprojects.gitakshmi.com/sso/callback",
 };
